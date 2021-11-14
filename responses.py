cb0e8490376f0a31781c67eec83f6401b80b642b
Akash <88738817+codedmachine111@users.noreply.github.com>
2021-11-14 13:25:37 +0530

    Update responses.py

--- responses.py
+++ responses.py
@@ -1,7 +1,7 @@
 def sample_resources(input_text):
     user_message= str(input_text).lower()
 
-    if user_message in ("line"):
-        return "to generate a new line type in /start"
+    if user_message in ("hey"):
+        return "Type /new to get my github"
 
     return "Sorry I dont understand"
